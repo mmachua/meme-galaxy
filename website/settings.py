ff6acf7bb42a2a4313e935ad547ac74c5a848a22
machuaa <machuamartin@gmail.com>
2022-07-11 15:42:33 +0300

    updated login urls

--- website/settings.py
+++ website/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'website.middleware.LoginRequiredMiddleware',
+    #'website.middleware.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'website.urls'
