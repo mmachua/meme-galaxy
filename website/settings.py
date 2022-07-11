ced6db13bec339ecbb39bcd71ee8ac0a5990f3ad
machuaa <machuamartin@gmail.com>
2022-07-11 15:21:46 +0300

    updated login urls

--- website/settings.py
+++ website/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'home',
     'login',
+    'home',
+    
     'hitcount',
     
     #'django.contrib.sites',
